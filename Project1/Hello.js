3923f9d3b8512593f7e194a8eeb54f98fac1f0ed
Aaron <aaroncvalentine2@gmail.com>
2026-08-27 13:25:40 -0600

    project1

---
+++ Project1/Hello.js
@@ -0,0 +1,11 @@
+
+        const button = document.getElementById('mainActionBtn');
+        button.addEventListener('click', function() {
+            const heading = document.querySelector('h1');
+            heading.style.color = '#e67e22';
+            const para = document.querySelector('p');
+            para.textContent = ' You just interacted with the page! Now add your own logic here.';
+
+            // Just a demo alert – remove this once you build your real interactivity
+            alert('Thank you for clicking my button I hope you feel a sense of fulfillment');
+        });
